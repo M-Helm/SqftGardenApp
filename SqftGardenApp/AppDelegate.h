2621f0845ff177ca0d87b157f87612922b0673ed
helm <matthew.helm@gmail.com>
2015-05-01 16:25:32 -0600

    Initial Commit

---
+++ SqftGardenApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqftGardenApp
+//
+//  Created by Matthew Helm on 5/1/15.
+//  Copyright (c) 2015 Matthew Helm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
