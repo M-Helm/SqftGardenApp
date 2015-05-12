74776c2a646a521335d3ecf5d390a910df97c1a5
helm <matthew.helm@gmail.com>
2015-05-12 08:33:08 -0600

    bedDimension sizing improved

--- SqftGardenApp/EditBedViewController.m
+++ SqftGardenApp/EditBedViewController.m
@@ -21,19 +21,6 @@
 
 UIView *bedFrameView;
 
-UIView *bed0;
-UIView *bed1;
-UIView *bed2;
-
-UIView *bed3;
-UIView *bed4;
-UIView *bed5;
-
-UIView *bed6;
-UIView *bed7;
-UIView *bed8;
-
-
 - (id)initWithDimensions:(int)rows columns:(int)columns {
     self.bedRowCount = rows;
     self.bedColumnCount = columns;
@@ -43,7 +30,7 @@ UIView *bed8;
 - (void)viewDidLoad {
     [super viewDidLoad];
     if((int)self.bedRowCount < 1)self.bedRowCount = 3;
-    if((int)self.bedColumnCount < 1)self.bedColumnCount = 3;
+    if((int)self.bedColumnCount < 1)self.bedColumnCount = 5;
     self.bedCellCount = self.bedRowCount * self.bedColumnCount;
     NSLog(@"Cell Count %i", (int)self.bedCellCount);
     self.bedViewArray = [[NSMutableArray alloc] init];
@@ -55,42 +42,9 @@ UIView *bed8;
 
 - (void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
-    //float xCo = self.view.bounds.size.width;
-    //float yCo = self.view.bounds.size.height;
     bedFrameView.layer.borderColor = [UIColor lightGrayColor].CGColor;
     bedFrameView.layer.borderWidth = 3;
     bedFrameView.layer.cornerRadius = 15;
-    [self.view addSubview:bedFrameView];
-    
-    /*
-    bed0.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed0.layer.borderWidth = 3;
-    bed0.layer.cornerRadius = 15;
-    bed1.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed1.layer.borderWidth = 3;
-    bed1.layer.cornerRadius = 15;
-    bed2.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed2.layer.borderWidth = 3;
-    bed2.layer.cornerRadius = 15;
-    bed3.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed3.layer.borderWidth = 3;
-    bed3.layer.cornerRadius = 15;
-    bed4.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed4.layer.borderWidth = 3;
-    bed4.layer.cornerRadius = 15;
-    bed5.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed5.layer.borderWidth = 3;
-    bed5.layer.cornerRadius = 15;
-    bed6.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed6.layer.borderWidth = 3;
-    bed6.layer.cornerRadius = 15;
-    bed7.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed7.layer.borderWidth = 3;
-    bed7.layer.cornerRadius = 15;
-    bed8.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    bed8.layer.borderWidth = 3;
-    bed8.layer.cornerRadius = 15;
-    */
     [self.view addSubview:bedFrameView];
     
 }
@@ -102,57 +56,29 @@ UIView *bed8;
 
 - (NSMutableArray *)buildBedViewArray{
     NSMutableArray *bedArray = [[NSMutableArray alloc] init];
-    int bedDimension = (int)(self.view.bounds.size.width - 20) / (int)self.bedRowCount;
+    int bedDimension = [self bedDimension];
     int rowNumber = 0;
     int columnNumber = 0;
-    for(int i=0; i<self.bedCellCount; i++){
+    for(int i=0; i<self.bedRowCount; i++){
         NSLog(@"Outer array loop");
-        while(rowNumber < self.bedRowCount){
-            BedView *bed = [[BedView alloc] initWithFrame:CGRectMake(1 + (bedDimension*rowNumber),
-                            (bedDimension*columnNumber)+1, bedDimension, bedDimension)];
+        while(columnNumber < self.bedColumnCount){
+            BedView *bed = [[BedView alloc] initWithFrame:CGRectMake(1 + (bedDimension*columnNumber),
+                            (bedDimension*rowNumber)+1, bedDimension, bedDimension)];
             [bedArray addObject:bed];
             NSLog(@"Inner array loop");
-            rowNumber++;
+            //rowNumber++;
+            columnNumber++;
         }
-        rowNumber = 0;
-        columnNumber++;
+        columnNumber = 0;
+        rowNumber++;
     }
     return bedArray;
 }
 
--(void)initViewsOLD{
-    float xCo = self.view.bounds.size.width;
-    //int bedDimension = (int)(self.view.bounds.size.width - 20);
-    int bedDimension = (int)(self.view.bounds.size.width - 20) / (int)self.bedRowCount;
-    int i = self.bedColumnCount * bedDimension;
-    bedFrameView = [[UIView alloc] initWithFrame:CGRectMake(10, 100, xCo-20, i+10)];
-    
-    bed0 = [[UIView alloc] initWithFrame:CGRectMake(1, 1, bedDimension, bedDimension)];
-    bed1 = [[UIView alloc] initWithFrame:CGRectMake(bedDimension+1, 1, bedDimension, bedDimension)];
-    bed2 = [[UIView alloc] initWithFrame:CGRectMake((bedDimension*2)+1, 1, bedDimension, bedDimension)];
-    
-    bed3 = [[UIView alloc] initWithFrame:CGRectMake(1, bedDimension+1, (bedDimension * 1) +1, bedDimension)];
-    bed4 = [[UIView alloc] initWithFrame:CGRectMake(bedDimension+1,(bedDimension * 1)+1, bedDimension, bedDimension)];
-    bed5 = [[UIView alloc] initWithFrame:CGRectMake((bedDimension * 2)+1, (bedDimension * 1)+1, bedDimension, bedDimension)];
-
-    bed6 = [[UIView alloc] initWithFrame:CGRectMake(1, (bedDimension * 2)+1, bedDimension, bedDimension)];
-    bed7 = [[UIView alloc] initWithFrame:CGRectMake(bedDimension+1,(bedDimension * 2)+1, bedDimension, bedDimension)];
-    bed8 = [[UIView alloc] initWithFrame:CGRectMake((bedDimension * 2)+1, (bedDimension * 2)+1, bedDimension, bedDimension)];
-    
-    [bedFrameView addSubview:bed0];
-    [bedFrameView addSubview:bed1];
-    [bedFrameView addSubview:bed2];
-    [bedFrameView addSubview:bed3];
-    [bedFrameView addSubview:bed4];
-    [bedFrameView addSubview:bed5];
-    [bedFrameView addSubview:bed6];
-    [bedFrameView addSubview:bed7];
-    [bedFrameView addSubview:bed8];
-}
 -(void)initViews{
     NSLog(@"initViews");
     float xCo = self.view.bounds.size.width;
-    int bedDimension = (int)(self.view.bounds.size.width - 20) / (int)self.bedRowCount;
+    int bedDimension = [self bedDimension];
     int i = self.bedColumnCount * bedDimension;
     bedFrameView = [[UIView alloc] initWithFrame:CGRectMake(10, 100, xCo-20, i+10)];
     NSLog(@"array count %i", self.bedViewArray.count);
@@ -161,8 +87,14 @@ UIView *bed8;
         NSLog(@"array add %i", i);
     }
 }
-
-
+-(int)bedDimension{
+    int columnDimension = (int)(self.view.bounds.size.width - 20) / (int)self.bedColumnCount;
+    int bedDimension = (int)(self.view.bounds.size.height - 60) / (int)self.bedRowCount;
+    if(bedDimension > columnDimension){
+        bedDimension = columnDimension;
+    }
+    return bedDimension;
+}
 
 @end
 
