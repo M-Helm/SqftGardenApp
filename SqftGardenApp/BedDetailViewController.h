a834e3b2cc1cccc5897c6b45a7ffd43438b13e86
helm <matthew.helm@gmail.com>
2015-05-13 10:35:56 -0600

    BedDetailVC added

---
+++ SqftGardenApp/BedDetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  BedDetailViewController.h
+//  SqftGardenApp
+//
+//  Created by Matthew Helm on 5/13/15.
+//  Copyright (c) 2015 Matthew Helm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BedDetailViewController : UIViewController
+
+@end
---
+++ SqftGardenApp/BedDetailViewController.m
@@ -0,0 +1,19 @@
+//
+//  BedDetailViewController.m
+//  SqftGardenApp
+//
+//  Created by Matthew Helm on 5/13/15.
+//  Copyright (c) 2015 Matthew Helm. All rights reserved.
+//
+
+#import "BedDetailViewController.h"
+
+@implementation BedDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //setup views
+}
+
+
+@end
