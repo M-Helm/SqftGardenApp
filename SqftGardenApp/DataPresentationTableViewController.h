7d3c819a57c61fd19f36e294f59005e882540083
helm <matthew.helm@gmail.com>
2015-09-22 13:31:52 -0600

    stubs for PresentationTableVC added

---
+++ SqftGardenApp/DataPresentationTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  DataPresentationTableViewController.h
+//  SqftGardenApp
+//
+//  Created by Matthew Helm on 9/22/15.
+//  Copyright © 2015 Matthew Helm. All rights reserved.
+//
+
+#import<UIKit/UIKit.h>
+
+@interface DataPresentationTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+
+@end
---
+++ SqftGardenApp/DataPresentationTableViewController.m
@@ -0,0 +1,55 @@
+//
+//  DataPresentationTableViewController.m
+//  SqftGardenApp
+//
+//  Created by Matthew Helm on 9/22/15.
+//  Copyright © 2015 Matthew Helm. All rights reserved.
+//
+
+#import "DataPresentationTableViewController.h"
+#import "DBManager.h"
+#import "ApplicationGlobals.h"
+
+@interface DataPresentationTableViewController()
+
+@end
+
+@implementation DataPresentationTableViewController
+
+DBManager *dbManager;
+ApplicationGlobals *appGlobals;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    dbManager = [DBManager getSharedDBManager];
+    appGlobals = [ApplicationGlobals getSharedGlobals];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *CellIdentifier;
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+}
+
+
+@end
